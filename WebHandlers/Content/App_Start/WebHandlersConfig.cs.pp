22e24a584b77c93243b657ebcfdd9f1ea9a00a5c
Chris Powell <powelcr@uc.edu>
2013-11-14 16:25:34 -0500

    Clean up naming

--- WebHandlers/Content/App_Start/WebHandlersConfig.cs.pp
+++ WebHandlers/Content/App_Start/WebHandlersConfig.cs.pp
@@ -4,10 +4,10 @@ using System.Web.Routing;
 
 
 [assembly: WebActivatorEx.PreApplicationStartMethod(
-    typeof($rootnamespace$.App_Start.WebHandlerPackage), "PreStart")]
+    typeof($rootnamespace$.App_Start.WebHandlerConfig), "PreStart")]
 
 namespace $rootnamespace$.App_Start {
-    public static class WebHandlerPackage {
+    public static class WebHandlerConfig {
         public static void PreStart() {
 			RouteTable.Routes.IgnoreRoute("{*allaxd}", new { allaxd = @".*\.axd(/.*)?" });
         }
--- WebHandlers/Content/App_Start/WebHandlersConfig.vb.pp
+++ WebHandlers/Content/App_Start/WebHandlersConfig.vb.pp
@@ -1,10 +1,10 @@
 ﻿imports System
 
 <assembly: WebActivatorEx.PreApplicationStartMethod(
-    gettype($rootnamespace$.App_Start.WebHandlerPackage), "PreStart")>
+    gettype($rootnamespace$.App_Start.WebHandlerConfig), "PreStart")>
 
 Namespace $rootnamespace$.App_Start 
-    public class WebHandlerPackage 
+    public class WebHandlerConfig 
         public shared sub PreStart() 
 			RouteTable.Routes.IgnoreRoute("{*allaxd}", New With {.allaxd = ".*\.axd(/.*)?"})
         end sub
